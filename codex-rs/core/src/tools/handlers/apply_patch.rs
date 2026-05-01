EmpiricaAI/ecodex

Support multi-env filesystem tools
Add environment selection to the remaining environment-backed filesystem tools. apply_patch, list_dir, and view_image now route through the selected turn environment when an environment_id is provided, while preserving primary-environment fallback.

Co-authored-by: Codex <noreply@openai.com>

codex-rs/core/src/tools/handlers/apply_patch.rs
@@ -22,6 +22,8 @@ use crate::tools::events::ToolEmitter;
 use crate::tools::events::ToolEventCtx;
 use crate::tools::handlers::apply_granted_turn_permissions;
 use crate::tools::handlers::parse_arguments;
+use crate::tools::handlers::resolve_environment_id_argument;
+use crate::tools::handlers::resolve_process_tool_environment;
 use crate::tools::hook_names::HookToolName;
 use crate::tools::orchestrator::ToolOrchestrator;
 use crate::tools::registry::PostToolUsePayload;
@@ -258,6 +260,7 @@ fn apply_patch_payload_command(payload: &ToolPayload) -> Option<String> {
 async fn effective_patch_permissions(
     session: &Session,
     turn: &TurnContext,
+    cwd: &AbsolutePathBuf,
     action: &ApplyPatchAction,
 ) -> (
     Vec<AbsolutePathBuf>,
@@ -276,9 +279,9 @@ async fn effective_patch_permissions(
     );
     let effective_additional_permissions = apply_granted_turn_permissions(
         session,
-        turn.cwd.as_path(),
+        cwd.as_path(),
         crate::sandboxing::SandboxPermissions::UseDefault,
-        write_permissions_for_paths(&file_paths, &file_system_sandbox_policy, &turn.cwd),
+        write_permissions_for_paths(&file_paths, &file_system_sandbox_policy, cwd),
     )
     .await;
 
@@ -346,12 +349,12 @@ impl ToolHandler for ApplyPatchHandler {
             ..
         } = invocation;
 
-        let patch_input = match payload {
+        let patch_input = match &payload {
             ToolPayload::Function { arguments } => {
                 let args: ApplyPatchToolArgs = parse_arguments(&arguments)?;
                 args.input
             }
-            ToolPayload::Custom { input } => input,
+            ToolPayload::Custom { input } => input.clone(),
             _ => {
                 return Err(FunctionCallError::RespondToModel(
                     "apply_patch handler received unsupported payload".to_string(),
@@ -361,18 +364,24 @@ impl ToolHandler for ApplyPatchHandler {
 
         // Re-parse and verify the patch so we can compute changes and approval.
         // Avoid building temporary ExecParams/command vectors; derive directly from inputs.
-        let cwd = turn.cwd.clone();
-        let command = vec!["apply_patch".to_string(), patch_input.clone()];
-        let Some(turn_environment) = turn.primary_environment() else {
+        let environment_id = match &payload {
+            ToolPayload::Function { arguments } => resolve_environment_id_argument(arguments)?,
+            ToolPayload::Custom { .. } => None,
+            _ => None,
+        };
+        let Some(process_environment) =
+            resolve_process_tool_environment(&turn, environment_id.as_deref())?
+        else {
             return Err(FunctionCallError::RespondToModel(
                 "apply_patch is unavailable in this session".to_string(),
             ));
         };
-        let fs = turn_environment.environment.get_filesystem();
-        let sandbox = turn_environment
-            .environment
-            .is_remote()
-            .then(|| turn.file_system_sandbox_context(/*additional_permissions*/ None));
+        let cwd = process_environment.cwd.clone();
+        let command = vec!["apply_patch".to_string(), patch_input.clone()];
+        let fs = process_environment.environment.get_filesystem();
+        let sandbox = process_environment.environment.is_remote().then(|| {
+            turn.file_system_sandbox_context_for_cwd(&cwd, /*additional_permissions*/ None)
+        });
         match codex_apply_patch::maybe_parse_apply_patch_verified(
             &command,
             &cwd,
@@ -383,7 +392,8 @@ impl ToolHandler for ApplyPatchHandler {
         {
             codex_apply_patch::MaybeApplyPatchVerified::Body(changes) => {
                 let (file_paths, effective_additional_permissions, file_system_sandbox_policy) =
-                    effective_patch_permissions(session.as_ref(), turn.as_ref(), &changes).await;
+                    effective_patch_permissions(session.as_ref(), turn.as_ref(), &cwd, &changes)
+                        .await;
                 match apply_patch::apply_patch(turn.as_ref(), &file_system_sandbox_policy, changes)
                     .await
                 {
@@ -405,6 +415,7 @@ impl ToolHandler for ApplyPatchHandler {
 
                         let req = ApplyPatchRequest {
                             action: apply.action,
+                            file_system: fs.clone(),
                             file_paths,
                             changes,
                             exec_approval_requirement: apply.exec_approval_requirement,
@@ -467,19 +478,21 @@ impl ToolHandler for ApplyPatchHandler {
 pub(crate) async fn intercept_apply_patch(
     command: &[String],
     cwd: &AbsolutePathBuf,
-    fs: &dyn ExecutorFileSystem,
+    fs: Arc<dyn ExecutorFileSystem>,
+    sandbox: Option<codex_exec_server::FileSystemSandboxContext>,
     session: Arc<Session>,
     turn: Arc<TurnContext>,
     tracker: Option<&SharedTurnDiffTracker>,
     call_id: &str,
     tool_name: &str,
 ) -> Result<Option<FunctionToolOutput>, FunctionCallError> {
-    let sandbox = turn
-        .primary_environment()
-        .filter(|env| env.environment.is_remote())
-        .map(|_| turn.file_system_sandbox_context(/*additional_permissions*/ None));
-    match codex_apply_patch::maybe_parse_apply_patch_verified(command, cwd, fs, sandbox.as_ref())
-        .await
+    match codex_apply_patch::maybe_parse_apply_patch_verified(
+        command,
+        cwd,
+        fs.as_ref(),
+        sandbox.as_ref(),
+    )
+    .await
     {
         codex_apply_patch::MaybeApplyPatchVerified::Body(changes) => {
             session
@@ -491,7 +504,7 @@ pub(crate) async fn intercept_apply_patch(
                 )
                 .await;
             let (approval_keys, effective_additional_permissions, file_system_sandbox_policy) =
-                effective_patch_permissions(session.as_ref(), turn.as_ref(), &changes).await;
+                effective_patch_permissions(session.as_ref(), turn.as_ref(), cwd, &changes).await;
             match apply_patch::apply_patch(turn.as_ref(), &file_system_sandbox_policy, changes)
                 .await
             {
@@ -512,6 +525,7 @@ pub(crate) async fn intercept_apply_patch(
 
                     let req = ApplyPatchRequest {
                         action: apply.action,
+                        file_system: fs.clone(),
                         file_paths: approval_keys,
                         changes,
                         exec_approval_requirement: apply.exec_approval_requirement,

codex-rs/core/src/tools/handlers/list_dir.rs
@@ -1,19 +1,24 @@
 use std::collections::VecDeque;
 use std::ffi::OsStr;
-use std::fs::FileType;
 use std::path::Path;
 use std::path::PathBuf;
+use std::sync::Arc;
 
+use codex_exec_server::ExecutorFileSystem;
+use codex_exec_server::FileMetadata;
+use codex_exec_server::FileSystemSandboxContext;
 use codex_protocol::permissions::ReadDenyMatcher;
+use codex_utils_absolute_path::AbsolutePathBuf;
 use codex_utils_string::take_bytes_at_char_boundary;
 use serde::Deserialize;
-use tokio::fs;
 
 use crate::function_tool::FunctionCallError;
 use crate::tools::context::FunctionToolOutput;
 use crate::tools::context::ToolInvocation;
 use crate::tools::context::ToolPayload;
 use crate::tools::handlers::parse_arguments;
+use crate::tools::handlers::resolve_environment_id_argument;
+use crate::tools::handlers::resolve_process_tool_environment;
 use crate::tools::registry::ToolHandler;
 use crate::tools::registry::ToolKind;
 
@@ -39,6 +44,8 @@ fn default_depth() -> usize {
 #[derive(Deserialize)]
 struct ListDirArgs {
     dir_path: String,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    environment_id: Option<String>,
     #[serde(default = "default_offset")]
     offset: usize,
     #[serde(default = "default_limit")]
@@ -66,10 +73,12 @@ impl ToolHandler for ListDirHandler {
             }
         };
 
+        let environment_id = resolve_environment_id_argument(&arguments)?;
         let args: ListDirArgs = parse_arguments(&arguments)?;
 
         let ListDirArgs {
             dir_path,
+            environment_id: _,
             offset,
             limit,
             depth,
@@ -94,16 +103,22 @@ impl ToolHandler for ListDirHandler {
         }
 
         let path = PathBuf::from(&dir_path);
-        if !path.is_absolute() {
+        let path = AbsolutePathBuf::from_absolute_path_checked(&path).map_err(|_| {
+            FunctionCallError::RespondToModel("dir_path must be an absolute path".to_string())
+        })?;
+        let Some(target_environment) =
+            resolve_process_tool_environment(&turn, environment_id.as_deref())?
+        else {
             return Err(FunctionCallError::RespondToModel(
-                "dir_path must be an absolute path".to_string(),
+                "list_dir is unavailable in this session".to_string(),
             ));
-        }
+        };
         let file_system_sandbox_policy = turn.file_system_sandbox_policy();
-        let read_deny_matcher = ReadDenyMatcher::new(&file_system_sandbox_policy, &turn.cwd);
+        let read_deny_matcher =
+            ReadDenyMatcher::new(&file_system_sandbox_policy, &target_environment.cwd);
         if read_deny_matcher
             .as_ref()
-            .is_some_and(|matcher| matcher.is_read_denied(&path))
+            .is_some_and(|matcher| matcher.is_read_denied(path.as_path()))
         {
             return Err(FunctionCallError::RespondToModel(format!(
                 "{DENY_READ_POLICY_MESSAGE}: `{}`",
@@ -111,9 +126,22 @@ impl ToolHandler for ListDirHandler {
             )));
         }
 
-        let entries =
-            list_dir_slice_with_policy(&path, offset, limit, depth, read_deny_matcher.as_ref())
-                .await?;
+        let sandbox = target_environment.environment.is_remote().then(|| {
+            turn.file_system_sandbox_context_for_cwd(
+                &target_environment.cwd,
+                /*additional_permissions*/ None,
+            )
+        });
+        let entries = list_dir_slice_with_policy(
+            target_environment.environment.get_filesystem(),
+            &path,
+            offset,
+            limit,
+            depth,
+            read_deny_matcher.as_ref(),
+            sandbox.as_ref(),
+        )
+        .await?;
         let mut output = Vec::with_capacity(entries.len() + 1);
         output.push(format!("Absolute path: {}", path.display()));
         output.extend(entries);
@@ -122,14 +150,25 @@ impl ToolHandler for ListDirHandler {
 }
 
 async fn list_dir_slice_with_policy(
-    path: &Path,
+    fs: Arc<dyn ExecutorFileSystem>,
+    path: &AbsolutePathBuf,
     offset: usize,
     limit: usize,
     depth: usize,
     read_deny_matcher: Option<&ReadDenyMatcher>,
+    sandbox: Option<&FileSystemSandboxContext>,
 ) -> Result<Vec<String>, FunctionCallError> {
     let mut entries = Vec::new();
-    collect_entries(path, Path::new(""), depth, read_deny_matcher, &mut entries).await?;
+    collect_entries(
+        fs,
+        path,
+        Path::new(""),
+        depth,
+        read_deny_matcher,
+        sandbox,
+        &mut entries,
+    )
+    .await?;
 
     if entries.is_empty() {
         return Ok(Vec::new());
@@ -162,47 +201,47 @@ async fn list_dir_slice_with_policy(
 }
 
 async fn collect_entries(
-    dir_path: &Path,
+    fs: Arc<dyn ExecutorFileSystem>,
+    dir_path: &AbsolutePathBuf,
     relative_prefix: &Path,
     depth: usize,
     read_deny_matcher: Option<&ReadDenyMatcher>,
+    sandbox: Option<&FileSystemSandboxContext>,
     entries: &mut Vec<DirEntry>,
 ) -> Result<(), FunctionCallError> {
     let mut queue = VecDeque::new();
-    queue.push_back((dir_path.to_path_buf(), relative_prefix.to_path_buf(), depth));
+    queue.push_back((dir_path.clone(), relative_prefix.to_path_buf(), depth));
 
     while let Some((current_dir, prefix, remaining_depth)) = queue.pop_front() {
-        let mut read_dir = fs::read_dir(&current_dir).await.map_err(|err| {
-            FunctionCallError::RespondToModel(format!("failed to read directory: {err}"))
-        })?;
-
         let mut dir_entries = Vec::new();
 
-        while let Some(entry) = read_dir.next_entry().await.map_err(|err| {
-            FunctionCallError::RespondToModel(format!("failed to read directory: {err}"))
-        })? {
-            let entry_path = entry.path();
+        for entry in fs
+            .read_directory(&current_dir, sandbox)
+            .await
+            .map_err(|err| {
+                FunctionCallError::RespondToModel(format!("failed to read directory: {err}"))
+            })?
+        {
+            let entry_path = current_dir.join(&entry.file_name);
             if let Some(read_deny_matcher) = read_deny_matcher
-                && read_deny_matcher.is_read_denied(&entry_path)
+                && read_deny_matcher.is_read_denied(entry_path.as_path())
             {
                 continue;
             }
-
-            let file_type = entry.file_type().await.map_err(|err| {
+            let metadata = fs.get_metadata(&entry_path, sandbox).await.map_err(|err| {
                 FunctionCallError::RespondToModel(format!("failed to inspect entry: {err}"))
             })?;
 
-            let file_name = entry.file_name();
             let relative_path = if prefix.as_os_str().is_empty() {
-                PathBuf::from(&file_name)
+                PathBuf::from(&entry.file_name)
             } else {
-                prefix.join(&file_name)
+                prefix.join(&entry.file_name)
             };
 
-            let display_name = format_entry_component(&file_name);
+            let display_name = format_entry_component(OsStr::new(&entry.file_name));
             let display_depth = prefix.components().count();
             let sort_key = format_entry_name(&relative_path);
-            let kind = DirEntryKind::from(&file_type);
+            let kind = DirEntryKind::from(&metadata);
             dir_entries.push((
                 entry_path,
                 relative_path,
@@ -275,13 +314,13 @@ enum DirEntryKind {
     Other,
 }
 
-impl From<&FileType> for DirEntryKind {
-    fn from(file_type: &FileType) -> Self {
-        if file_type.is_symlink() {
+impl From<&FileMetadata> for DirEntryKind {
+    fn from(metadata: &FileMetadata) -> Self {
+        if metadata.is_symlink {
             DirEntryKind::Symlink
-        } else if file_type.is_dir() {
+        } else if metadata.is_directory {
             DirEntryKind::Directory
-        } else if file_type.is_file() {
+        } else if metadata.is_file {
             DirEntryKind::File
         } else {
             DirEntryKind::Other

agentserver/codex

codex-rs/core/src/tools/handlers/apply_patch.rs
@@ -22,6 +22,7 @@ use crate::tools::events::ToolEmitter;
 use crate::tools::events::ToolEventCtx;
 use crate::tools::handlers::apply_granted_turn_permissions;
 use crate::tools::handlers::parse_arguments;
+use crate::tools::handlers::resolve_tool_call_environment;
 use crate::tools::hook_names::HookToolName;
 use crate::tools::orchestrator::ToolOrchestrator;
 use crate::tools::registry::PostToolUsePayload;
@@ -258,6 +259,7 @@ fn apply_patch_payload_command(payload: &ToolPayload) -> Option<String> {
 async fn effective_patch_permissions(
     session: &Session,
     turn: &TurnContext,
+    cwd: &AbsolutePathBuf,
     action: &ApplyPatchAction,
 ) -> (
     Vec<AbsolutePathBuf>,
@@ -276,9 +278,9 @@ async fn effective_patch_permissions(
     );
     let effective_additional_permissions = apply_granted_turn_permissions(
         session,
-        turn.cwd.as_path(),
+        cwd.as_path(),
         crate::sandboxing::SandboxPermissions::UseDefault,
-        write_permissions_for_paths(&file_paths, &file_system_sandbox_policy, &turn.cwd),
+        write_permissions_for_paths(&file_paths, &file_system_sandbox_policy, cwd),
     )
     .await;
 
@@ -346,12 +348,12 @@ impl ToolHandler for ApplyPatchHandler {
             ..
         } = invocation;
 
-        let patch_input = match payload {
+        let (patch_input, environment_id) = match &payload {
             ToolPayload::Function { arguments } => {
                 let args: ApplyPatchToolArgs = parse_arguments(&arguments)?;
-                args.input
+                (args.input, args.environment_id)
             }
-            ToolPayload::Custom { input } => input,
+            ToolPayload::Custom { input } => (input.clone(), None),
             _ => {
                 return Err(FunctionCallError::RespondToModel(
                     "apply_patch handler received unsupported payload".to_string(),
@@ -361,18 +363,18 @@ impl ToolHandler for ApplyPatchHandler {
 
         // Re-parse and verify the patch so we can compute changes and approval.
         // Avoid building temporary ExecParams/command vectors; derive directly from inputs.
-        let cwd = turn.cwd.clone();
-        let command = vec!["apply_patch".to_string(), patch_input.clone()];
-        let Some(turn_environment) = turn.environments.primary() else {
+        let Some(environment) = resolve_tool_call_environment(&turn, environment_id.as_deref())?
+        else {
             return Err(FunctionCallError::RespondToModel(
                 "apply_patch is unavailable in this session".to_string(),
             ));
         };
-        let fs = turn_environment.environment.get_filesystem();
-        let sandbox = turn_environment
-            .environment
-            .is_remote()
-            .then(|| turn.file_system_sandbox_context(/*additional_permissions*/ None));
+        let cwd = environment.cwd.clone();
+        let command = vec!["apply_patch".to_string(), patch_input.clone()];
+        let fs = environment.environment.get_filesystem();
+        let sandbox = environment.environment.is_remote().then(|| {
+            turn.file_system_sandbox_context_for_cwd(&cwd, /*additional_permissions*/ None)
+        });
         match codex_apply_patch::maybe_parse_apply_patch_verified(
             &command,
             &cwd,
@@ -383,7 +385,8 @@ impl ToolHandler for ApplyPatchHandler {
         {
             codex_apply_patch::MaybeApplyPatchVerified::Body(changes) => {
                 let (file_paths, effective_additional_permissions, file_system_sandbox_policy) =
-                    effective_patch_permissions(session.as_ref(), turn.as_ref(), &changes).await;
+                    effective_patch_permissions(session.as_ref(), turn.as_ref(), &cwd, &changes)
+                        .await;
                 match apply_patch::apply_patch(turn.as_ref(), &file_system_sandbox_policy, changes)
                     .await
                 {
@@ -405,6 +408,7 @@ impl ToolHandler for ApplyPatchHandler {
 
                         let req = ApplyPatchRequest {
                             action: apply.action,
+                            file_system: fs.clone(),
                             file_paths,
                             changes,
                             exec_approval_requirement: apply.exec_approval_requirement,
@@ -467,20 +471,21 @@ impl ToolHandler for ApplyPatchHandler {
 pub(crate) async fn intercept_apply_patch(
     command: &[String],
     cwd: &AbsolutePathBuf,
-    fs: &dyn ExecutorFileSystem,
+    fs: Arc<dyn ExecutorFileSystem>,
+    sandbox: Option<codex_exec_server::FileSystemSandboxContext>,
     session: Arc<Session>,
     turn: Arc<TurnContext>,
     tracker: Option<&SharedTurnDiffTracker>,
     call_id: &str,
     tool_name: &str,
 ) -> Result<Option<FunctionToolOutput>, FunctionCallError> {
-    let sandbox = turn
-        .environments
-        .primary()
-        .filter(|env| env.environment.is_remote())
-        .map(|_| turn.file_system_sandbox_context(/*additional_permissions*/ None));
-    match codex_apply_patch::maybe_parse_apply_patch_verified(command, cwd, fs, sandbox.as_ref())
-        .await
+    match codex_apply_patch::maybe_parse_apply_patch_verified(
+        command,
+        cwd,
+        fs.as_ref(),
+        sandbox.as_ref(),
+    )
+    .await
     {
         codex_apply_patch::MaybeApplyPatchVerified::Body(changes) => {
             session
@@ -492,7 +497,7 @@ pub(crate) async fn intercept_apply_patch(
                 )
                 .await;
             let (approval_keys, effective_additional_permissions, file_system_sandbox_policy) =
-                effective_patch_permissions(session.as_ref(), turn.as_ref(), &changes).await;
+                effective_patch_permissions(session.as_ref(), turn.as_ref(), cwd, &changes).await;
             match apply_patch::apply_patch(turn.as_ref(), &file_system_sandbox_policy, changes)
                 .await
             {
@@ -513,6 +518,7 @@ pub(crate) async fn intercept_apply_patch(
 
                     let req = ApplyPatchRequest {
                         action: apply.action,
+                        file_system: fs.clone(),
                         file_paths: approval_keys,
                         changes,
                         exec_approval_requirement: apply.exec_approval_requirement,

codex-rs/core/src/tools/handlers/list_dir.rs
@@ -1,19 +1,23 @@
 use std::collections::VecDeque;
 use std::ffi::OsStr;
-use std::fs::FileType;
 use std::path::Path;
 use std::path::PathBuf;
+use std::sync::Arc;
 
+use codex_exec_server::ExecutorFileSystem;
+use codex_exec_server::FileMetadata;
+use codex_exec_server::FileSystemSandboxContext;
 use codex_protocol::permissions::ReadDenyMatcher;
+use codex_utils_absolute_path::AbsolutePathBuf;
 use codex_utils_string::take_bytes_at_char_boundary;
 use serde::Deserialize;
-use tokio::fs;
 
 use crate::function_tool::FunctionCallError;
 use crate::tools::context::FunctionToolOutput;
 use crate::tools::context::ToolInvocation;
 use crate::tools::context::ToolPayload;
 use crate::tools::handlers::parse_arguments;
+use crate::tools::handlers::resolve_tool_call_environment;
 use crate::tools::registry::ToolHandler;
 use crate::tools::registry::ToolKind;
 
@@ -39,6 +43,8 @@ fn default_depth() -> usize {
 #[derive(Deserialize)]
 struct ListDirArgs {
     dir_path: String,
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    environment_id: Option<String>,
     #[serde(default = "default_offset")]
     offset: usize,
     #[serde(default = "default_limit")]
@@ -70,6 +76,7 @@ impl ToolHandler for ListDirHandler {
 
         let ListDirArgs {
             dir_path,
+            environment_id,
             offset,
             limit,
             depth,
@@ -94,16 +101,22 @@ impl ToolHandler for ListDirHandler {
         }
 
         let path = PathBuf::from(&dir_path);
-        if !path.is_absolute() {
+        let path = AbsolutePathBuf::from_absolute_path_checked(&path).map_err(|_| {
+            FunctionCallError::RespondToModel("dir_path must be an absolute path".to_string())
+        })?;
+        let Some(target_environment) =
+            resolve_tool_call_environment(&turn, environment_id.as_deref())?
+        else {
             return Err(FunctionCallError::RespondToModel(
-                "dir_path must be an absolute path".to_string(),
+                "list_dir is unavailable in this session".to_string(),
             ));
-        }
+        };
         let file_system_sandbox_policy = turn.file_system_sandbox_policy();
-        let read_deny_matcher = ReadDenyMatcher::new(&file_system_sandbox_policy, &turn.cwd);
+        let read_deny_matcher =
+            ReadDenyMatcher::new(&file_system_sandbox_policy, &target_environment.cwd);
         if read_deny_matcher
             .as_ref()
-            .is_some_and(|matcher| matcher.is_read_denied(&path))
+            .is_some_and(|matcher| matcher.is_read_denied(path.as_path()))
         {
             return Err(FunctionCallError::RespondToModel(format!(
                 "{DENY_READ_POLICY_MESSAGE}: `{}`",
@@ -111,9 +124,22 @@ impl ToolHandler for ListDirHandler {
             )));
         }
 
-        let entries =
-            list_dir_slice_with_policy(&path, offset, limit, depth, read_deny_matcher.as_ref())
-                .await?;
+        let sandbox = target_environment.environment.is_remote().then(|| {
+            turn.file_system_sandbox_context_for_cwd(
+                &target_environment.cwd,
+                /*additional_permissions*/ None,
+            )
+        });
+        let entries = list_dir_slice_with_policy(
+            target_environment.environment.get_filesystem(),
+            &path,
+            offset,
+            limit,
+            depth,
+            read_deny_matcher.as_ref(),
+            sandbox.as_ref(),
+        )
+        .await?;
         let mut output = Vec::with_capacity(entries.len() + 1);
         output.push(format!("Absolute path: {}", path.display()));
         output.extend(entries);
@@ -122,14 +148,25 @@ impl ToolHandler for ListDirHandler {
 }
 
 async fn list_dir_slice_with_policy(
-    path: &Path,
+    fs: Arc<dyn ExecutorFileSystem>,
+    path: &AbsolutePathBuf,
     offset: usize,
     limit: usize,
     depth: usize,
     read_deny_matcher: Option<&ReadDenyMatcher>,
+    sandbox: Option<&FileSystemSandboxContext>,
 ) -> Result<Vec<String>, FunctionCallError> {
     let mut entries = Vec::new();
-    collect_entries(path, Path::new(""), depth, read_deny_matcher, &mut entries).await?;
+    collect_entries(
+        fs,
+        path,
+        Path::new(""),
+        depth,
+        read_deny_matcher,
+        sandbox,
+        &mut entries,
+    )
+    .await?;
 
     if entries.is_empty() {
         return Ok(Vec::new());
@@ -162,47 +199,47 @@ async fn list_dir_slice_with_policy(
 }
 
 async fn collect_entries(
-    dir_path: &Path,
+    fs: Arc<dyn ExecutorFileSystem>,
+    dir_path: &AbsolutePathBuf,
     relative_prefix: &Path,
     depth: usize,
     read_deny_matcher: Option<&ReadDenyMatcher>,
+    sandbox: Option<&FileSystemSandboxContext>,
     entries: &mut Vec<DirEntry>,
 ) -> Result<(), FunctionCallError> {
     let mut queue = VecDeque::new();
-    queue.push_back((dir_path.to_path_buf(), relative_prefix.to_path_buf(), depth));
+    queue.push_back((dir_path.clone(), relative_prefix.to_path_buf(), depth));
 
     while let Some((current_dir, prefix, remaining_depth)) = queue.pop_front() {
-        let mut read_dir = fs::read_dir(&current_dir).await.map_err(|err| {
-            FunctionCallError::RespondToModel(format!("failed to read directory: {err}"))
-        })?;
-
         let mut dir_entries = Vec::new();
 
-        while let Some(entry) = read_dir.next_entry().await.map_err(|err| {
-            FunctionCallError::RespondToModel(format!("failed to read directory: {err}"))
-        })? {
-            let entry_path = entry.path();
+        for entry in fs
+            .read_directory(&current_dir, sandbox)
+            .await
+            .map_err(|err| {
+                FunctionCallError::RespondToModel(format!("failed to read directory: {err}"))
+            })?
+        {
+            let entry_path = current_dir.join(&entry.file_name);
             if let Some(read_deny_matcher) = read_deny_matcher
-                && read_deny_matcher.is_read_denied(&entry_path)
+                && read_deny_matcher.is_read_denied(entry_path.as_path())
             {
                 continue;
             }
-
-            let file_type = entry.file_type().await.map_err(|err| {
+            let metadata = fs.get_metadata(&entry_path, sandbox).await.map_err(|err| {
                 FunctionCallError::RespondToModel(format!("failed to inspect entry: {err}"))
             })?;
 
-            let file_name = entry.file_name();
             let relative_path = if prefix.as_os_str().is_empty() {
-                PathBuf::from(&file_name)
+                PathBuf::from(&entry.file_name)
             } else {
-                prefix.join(&file_name)
+                prefix.join(&entry.file_name)
             };
 
-            let display_name = format_entry_component(&file_name);
+            let display_name = format_entry_component(OsStr::new(&entry.file_name));
             let display_depth = prefix.components().count();
             let sort_key = format_entry_name(&relative_path);
-            let kind = DirEntryKind::from(&file_type);
+            let kind = DirEntryKind::from(&metadata);
             dir_entries.push((
                 entry_path,
                 relative_path,
@@ -275,13 +312,13 @@ enum DirEntryKind {
     Other,
 }
 
-impl From<&FileType> for DirEntryKind {
-    fn from(file_type: &FileType) -> Self {
-        if file_type.is_symlink() {
+impl From<&FileMetadata> for DirEntryKind {
+    fn from(metadata: &FileMetadata) -> Self {
+        if metadata.is_symlink {
             DirEntryKind::Symlink
-        } else if file_type.is_dir() {
+        } else if metadata.is_directory {
             DirEntryKind::Directory
-        } else if file_type.is_file() {
+        } else if metadata.is_file {
             DirEntryKind::File
         } else {
             DirEntryKind::Other